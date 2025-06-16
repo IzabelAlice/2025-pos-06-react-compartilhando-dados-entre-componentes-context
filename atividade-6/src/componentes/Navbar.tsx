a9ec73012e24200a26962be54a3d0b571b0c3b5f
Izabel Alice <124362770+IzabelAlice@users.noreply.github.com>
2025-06-16 13:29:48 +0000

    atualizando contraste da navbar

--- atividade-6/src/componentes/Navbar.tsx
+++ atividade-6/src/componentes/Navbar.tsx
@@ -1,12 +1,12 @@
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between px-30 py-4 border-gray-900 text-white">
+    <nav className="flex items-center justify-between px-30 py-4 border-gray-900 text-white bg-sky-900">
       <div className="font-bold text-xl">
         <h1 className="text-semibold">Tarefas</h1>
       </div>
       <div className="flex gap-4">
-        <a href="/tarefas" className="hover:underline">Tarefas</a>
-        <a href="/tarefas/nova" className="hover:underline">Nova Tarefa</a>
+        <a href="/tarefas" className="no-underline bg-zinc-400 p-2 px-4 rounded text-black ">Tarefas</a>
+        <a href="/tarefas/nova" className="no-underline bg-zinc-400 p-2 px-4 rounded text-black ">Nova Tarefa</a>
       </div>
     </nav>
   );
